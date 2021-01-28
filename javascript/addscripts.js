2430c23f509663768c8d648b61202daa953c2181
Calle <calle.alexanderson@elev.ga.ntig.se>
2021-01-28 16:28:48 +0100

    Starting with functions

--- javascript/addscripts.js
+++ javascript/addscripts.js
@@ -1,3 +1,18 @@
+"using strict";
+
 function dropdownFunction() {
-    document.getElementById("type-menu").classList.toggle("show");
-  }
+  document.getElementById("type-menu").classList.toggle("show");
+}
+  
+let currentlySelcetedMedia;
+let choices = document.querySelectorAll("input[type='radio']");
+let button = document.querySelector("#button1");
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", selectDifferentMedia);
+});
+
+function selectDifferentMedia(e){
+  currentlySelcetedMedia = e.target.value;
+  button.innerHTML = currentlySelcetedMedia;
+}
