660495a53694f937e31f94107e9923595e23bc20
Calle <calle.alexanderson@elev.ga.ntig.se>
2021-01-29 09:37:49 +0100

    score menu

--- javascript/addscripts.js
+++ javascript/addscripts.js
@@ -1,18 +1,34 @@
 "using strict";
 
-function dropdownFunction() {
+let currentlySelcetedMedia;
+//hittar alla radio på sidan
+let choices = document.querySelectorAll("input[type='radio']");
+let button = document.querySelector("#button1");let currentlySelcetedChapters;
+let currentlySelcetedScore;
+let currentlySelcetedComplete;
+let mediaList = [];
+
+//visar min dropdown meny när jag klickar på knappen
+function dropdownFunction1() {
   document.getElementById("type-menu").classList.toggle("show");
 }
-  
-let currentlySelcetedMedia;
-let choices = document.querySelectorAll("input[type='radio']");
-let button = document.querySelector("#button1");
 
+//gör ett event när man klickar i en radio
 choices.forEach((choice) => {
   choice.addEventListener("click", selectDifferentMedia);
 });
 
+//
 function selectDifferentMedia(e){
   currentlySelcetedMedia = e.target.value;
   button.innerHTML = currentlySelcetedMedia;
+}
+
+
+let currentlySelcetedMediaName = {
+  
+}
+
+function addToList(){
+  mediaList.push(mediaName);
 }
