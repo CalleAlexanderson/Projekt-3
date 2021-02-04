270b7827d3b755cbd6d911b3abcb3a67fff60b6e
Calle <calle.alexanderson@elev.ga.ntig.se>
2021-02-04 16:29:55 +0100

    Nästsita med add funktionen

--- javascript/addscripts.js
+++ javascript/addscripts.js
@@ -1,16 +1,21 @@
 "using strict";
 
+
+
 let currentlySelcetedMedia;
 //hittar alla radio på sidan
 let choices = document.querySelectorAll("input[type='radio']");
-let button = document.querySelector("#button1");let currentlySelcetedChapters;
-let currentlySelcetedScore;
-let currentlySelcetedComplete;
+let button = document.querySelector("#button1");
+let currentlySelcetedChapters = document.getElementById("media-title");
+let currentlySelcetedScore = document.getElementById("scores");
+let currentlySelcetedComplete = document.getElementById("complete-box");
+let currentlySelcetedMediaName = document.getElementById("title");
 let mediaList = [];
 
 //visar min dropdown meny när jag klickar på knappen
 function dropdownFunction1() {
   document.getElementById("type-menu").classList.toggle("show");
+  console.log(currentlySelcetedScore);
 }
 
 //gör ett event när man klickar i en radio
@@ -18,16 +23,14 @@ choices.forEach((choice) => {
   choice.addEventListener("click", selectDifferentMedia);
 });
 
-//
+//Ändrar knappens text beroende på radio 
 function selectDifferentMedia(e){
   currentlySelcetedMedia = e.target.value;
   button.innerHTML = currentlySelcetedMedia;
 }
 
 
-let currentlySelcetedMediaName = {
-  
-}
+
 
 function addToList(){
   mediaList.push(mediaName);
